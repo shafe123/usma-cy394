c352ada5054395ff2d5bd7bfe939857801fe5b33
Ethan Shafer <ethan.h.shafer@gmail.com>
2024-01-21 15:36:40 -0500

    Updated install script

--- hw1/install_script.sh
+++ hw1/install_script.sh
@@ -1,3 +1,5 @@
-apt install python-is-python3 python3-pip -y
-git clone https://github.com/shafe123/usma-cy394.git
-cd usma-cy394/hw1/flaskr
+cd usma-cy394/hw1
+apt install python-is-python3 python3-pip python3-flask -y
+pip3 install flask
+flask --app flaskr init-db
+flask --app flaskr --host 0.0.0.0 run
