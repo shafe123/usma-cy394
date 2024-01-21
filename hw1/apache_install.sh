74c8fdf08617f32f707359dbec6d8e3ae6be03bb
Ethan Shafer <ethan.h.shafer@gmail.com>
2024-01-21 16:37:03 -0500

    Added apache install

---
+++ hw1/apache_install.sh
@@ -0,0 +1,21 @@
+sudo apt install apache2 -y
+sudo a2enmod proxy_http lbmethod_bytraffic
+read -p "Enter the flask IP address: " flaskip
+echo "<VirtualHost *:80>
+    ServerName cy394.com
+
+    <Proxy balancer://flaskbalancer/>
+        BalancerMember http://$flaskip:5000/
+        ProxySet lbmethod=bytraffic
+    </Proxy>
+
+    ProxyPass "/"  "balancer://flaskbalancer/"
+    ProxyPassReverse "/"  "balancer://flaskbalancer/"
+</VirtualHost>
+
+<Location "/balancer-manager">
+    SetHandler balancer-manager
+    Require host localhost
+</Location>" | sudo tee -a /etc/apache2/sites-available/flask.conf
+sudo ln -s /etc/apache2/sites-available/flask.conf /etc/apache2/sites-enabled/flask.conf
+sudo systemctl restart apache2
--- hw1/flask_install.sh
+++ hw1/flask_install.sh
@@ -2,4 +2,5 @@ sudo apt install python-is-python3 python3-pip python3-flask -y
 cd usma-cy394/hw1
 pip3 install flask
 flask --app flaskr init-db
-flask --app flaskr --host 0.0.0.0 run
+flask --app flaskr run --host 0.0.0.0
+cd ~
