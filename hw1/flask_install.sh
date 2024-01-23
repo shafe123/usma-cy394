056b6dbbf6d2aae48157c0fc22ad1ac155acdc0c
Stephen Hamilton <stephenshamilton@gmail.com>
2024-01-23 15:56:21 -0500

    Update flask_install.sh removed apt install of flask
    this ensured the correct version of flask is installed through pip

--- hw1/flask_install.sh
+++ hw1/flask_install.sh
@@ -1,6 +1,6 @@
-sudo apt install python-is-python3 python3-pip python3-flask -y
+sudo apt install python-is-python3 python3-pip -y
 cd usma-cy394/hw1
 pip3 install flask
-flask --app flaskr init-db
-flask --app flaskr run --host 0.0.0.0
+python -m flask --app flaskr init-db
+python -m flask --app flaskr run --host 0.0.0.0
 cd ~
