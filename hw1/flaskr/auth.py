0f1d702fd5aee61605fbbf91c9c3439dc3d9554b
stephenshamilton@gmail.com <stephenshamilton@gmail.com>
2024-01-24 21:30:05 +0000

    updated blog to work with mysql

--- hw1/flaskr/auth.py
+++ hw1/flaskr/auth.py
@@ -12,6 +12,7 @@ from werkzeug.security import check_password_hash
 from werkzeug.security import generate_password_hash
 
 from flaskr.db import get_db
+import pdb
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
@@ -34,12 +35,17 @@ def load_logged_in_user():
     """If a user id is stored in the session, load the user object from
     the database into ``g.user``."""
     user_id = session.get("user_id")
-
+    db = get_db()
+    cursor = db.cursor()
+    
+    #u = cursor.fetchone()
+    pdb.set_trace()
     if user_id is None:
         g.user = None
     else:
+        #pdb.set_trace()
         g.user = (
-            get_db().execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
+            cursor.execute("SELECT * FROM user WHERE id = %u", u.id)
         )
 
 
@@ -54,6 +60,7 @@ def register():
         username = request.form["username"]
         password = request.form["password"]
         db = get_db()
+        cursor = db.cursor()
         error = None
 
         if not username:
@@ -63,14 +70,16 @@ def register():
 
         if error is None:
             try:
-                db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                cursor.execute(
+                    "INSERT INTO user (username, password) VALUES (%s, %s)",
                     (username, generate_password_hash(password)),
                 )
+                #cursor.commit()
                 db.commit()
-            except db.IntegrityError:
+            except Exception as e:
                 # The username was already taken, which caused the
                 # commit to fail. Show a validation error.
+                print ("error: %s", e)
                 error = f"User {username} is already registered."
             else:
                 # Success, go to the login page.
@@ -88,20 +97,22 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
         db = get_db()
+        cursor = db.cursor(named_tuple=True)
         error = None
-        user = db.execute(
-            "SELECT * FROM user WHERE username = ?", (username,)
-        ).fetchone()
-
+        cursor.execute(
+            "SELECT * FROM user WHERE username = %s", (username,)
+        )
+        user = cursor.fetchone()
+        pdb.set_trace()
         if user is None:
             error = "Incorrect username."
-        elif not check_password_hash(user["password"], password):
+        elif not check_password_hash(user.password, password):
             error = "Incorrect password."
 
         if error is None:
             # store the user id in a new session and return to the index
             session.clear()
-            session["user_id"] = user["id"]
+            session["user_id"] = user.id
             return redirect(url_for("index"))
 
         flash(error)
