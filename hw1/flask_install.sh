c818e1a2c2959d5bb5b550bb72eb623156b9e60d
Ethan Shafer <ethan.h.shafer@gmail.com>
2025-01-24 10:37:28 -0500

    Pin mysql-connector-python version

--- hw1/flask_install.sh
+++ hw1/flask_install.sh
@@ -2,5 +2,5 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install python-is-python3 python3-pip -y
 cd usma-cy394/hw1
-pip3 install flask mysql-connector-python
+pip3 install flask mysql-connector-python==9.1.0
 cd ~
